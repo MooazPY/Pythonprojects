57989c6b3b4c5b6cffe943d3ffe33939d0286c9d
MooazPY <119746071+MooazPY@users.noreply.github.com>
2024-07-28 20:02:17 +0300

    Update Higher-Lower.py

--- Higher-Lower Game/Higher-Lower.py
+++ Higher-Lower Game/Higher-Lower.py
@@ -2,16 +2,19 @@ from game_data import data
 import random
 from Game_Arts import *
 
+
 def generate_random_famous():
     """Gets random famous persons from our data list"""
     famous = random.choice(data)
     return famous
 
+
 def get_famous_score(famous):
     """Return number of followers of the random famous person"""
     return famous["follower_count"]
 
-def show_famous_data(famous1,famous2):
+
+def show_famous_data(famous1, famous2):
     """Show the famous data from our data list"""
     famous1_name = famous1['name']
     famous1_desc = famous1['description']
@@ -23,7 +26,8 @@ def show_famous_data(famous1,famous2):
     famous2_country = famous2['country']
     print(f"Against B: {famous2_name}, {famous2_desc}, from {famous2_country}")
 
-def check_winner(famous1,famous2,user_guess):
+
+def check_winner(famous1, famous2, user_guess):
     """Check is user's answer is correct or not"""
     if user_guess.upper() == 'A':
         user_guess = famous1
@@ -40,6 +44,7 @@ def check_winner(famous1,famous2,user_guess):
     else:
         return 0
 
+
 def play_game():
     print(logo)
     Flag = True
@@ -51,16 +56,21 @@ def play_game():
 
         while fam1 == fam2:
             fam2 = generate_random_famous()
+
         
-        show_famous_data(fam1,fam2)
+        show_famous_data(fam1, fam2)
         
         user_guess = input("Who has more followers? Type 'A' or 'B':" )
+
+        
         if check_winner(famous1=fam1,famous2=fam2,user_guess=user_guess) != 0:
             score += 1
             print(f"You're right! Current score: {score}")
         else:
             print(f"Sorry, that's wrong. Final score: {score}")
             Flag = False
+        
+        
         fam1 = fam2
         
 
