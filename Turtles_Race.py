3198d69041a2f632fee59c1637ee80c6e3526ad2
MooazPY <119746071+MooazPY@users.noreply.github.com>
2024-08-02 06:20:12 +0300

    Update Turtles_Race.py

--- Turtles_Race.py
+++ Turtles_Race.py
@@ -3,13 +3,15 @@ import random
 
 screen = Screen()
 screen.setup(width=500,height=400)
-user_guess =  screen.textinput("لعبه سباق السلاحف", "Which turtle gonna win: ")
+user_guess =  screen.textinput("Predict the winner", "Which turtle gonna win: ")
 
 start_y = -100
 start_x = -230
 
 list_of_turtles = []
 list_of_colors = ["red","orange","yellow","green","purple","blue"]
+
+
 for i in list_of_colors:
     new_turtle = Turtle()
     new_turtle.penup()
@@ -23,9 +25,11 @@ flag = True
 
 while flag:
     for i in range(len(list_of_turtles)):
+        
         current_turtle = list_of_turtles[i]
         current_turtle.forward(random.randint(1,10))
         current_turtle_x_axis = current_turtle.xcor()
+       
         if current_turtle_x_axis >= 220:
             winner = current_turtle.pencolor()
             print(f"The winner is the {current_turtle.pencolor()} turtle")
