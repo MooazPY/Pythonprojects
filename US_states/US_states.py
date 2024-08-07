e00aa5d41f8d9018dcb9fb2778b7000d01ea9183
Mooaz Rafaat <mooazrafaat11@gmail.com>
2024-08-07 09:12:34 +0300

    Add files via upload

---
+++ US_states/US_states.py
@@ -0,0 +1,45 @@
+from turtle import Screen , Turtle
+import pandas as pd
+
+screen = Screen()
+screen.setup(725,491)
+screen.bgpic(r'C:/Users/makka\Desktop\blank_states_img.gif')
+screen.title("US-STATES")
+
+data = pd.read_csv(r'C:/Users/makka\Desktop/50_states.csv')
+states = data['state'].to_list()
+state_axis = list(zip(data['x'],data['y']))
+# already_guessed = []
+
+answer = Turtle()
+answer.penup()
+answer.hideturtle()
+answer.pencolor("black")
+answer.pensize(2)
+answer.speed("normal")
+
+score = 0
+game_is_on = True
+while game_is_on:
+        user_answers = screen.textinput(f"{score}/50 States Correct", "Name a state in US:").title()
+        if score == len(states):
+                print("You win!!!")
+                game_is_on = False
+        if user_answers in states:
+                score += 1
+                state_index = states.index(user_answers)
+                answer.goto(state_axis[state_index])
+                answer.write(states[state_index],False)
+                states.remove(user_answers)
+                state_axis.remove(state_axis[state_index])
+
+        if user_answers.lower() == 'stop':
+                game_is_on = False
+                with open("missed_states","w") as miss:
+                        for missed in states:
+                                miss.write(f'{missed}\n')
+        answer.goto(0,0)
+
+
+
+screen.exitonclick()
